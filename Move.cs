1d3114e14f1e6ac907da1903bbb9bd53f6964766
Marco Matamoros <marco.matamoros@mail.utoronto.ca>
2019-10-15 14:31:34 -0400

    Initial commit

---
+++ Move.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public delegate void CompletionHandler();
+    public float speedScale = 1;
+
+    public void MoveTo(Vector3 targetPos, float time,
+                             CompletionHandler handler = null)
+    {
+        StartCoroutine(MoveToCoroutine(targetPos, time, handler));
+    }
+
+    public void MoveBy(Vector3 velocity, float distance,
+                             CompletionHandler handler = null)
+    {
+        StartCoroutine(MoveByCoroutine(velocity, distance, handler));
+    }
+
+    public void MoveBy(Vector3 velocity)
+    {
+        StartCoroutine(MoveByCoroutine(velocity));
+    }
+
+    private IEnumerator MoveToCoroutine(Vector3 targetPos, float time, CompletionHandler handler)
+    {
+
+        var startingPosition = gameObject.transform.localPosition;
+        var t = 0f;
+
+        while (gameObject.transform.localPosition != targetPos)
+        {
+            var scaledTime = time / speedScale;
+
+            t += Time.deltaTime / scaledTime ;
+            gameObject.transform.localPosition = Vector3.Lerp(startingPosition, targetPos, t);
+
+            yield return null;
+        }
+
+        if (handler != null)
+        {
+            handler();
+        }
+    }
+
+    private IEnumerator MoveByCoroutine(Vector3 velocity, float distance, CompletionHandler handler)
+    {
+        var startingPosition = gameObject.transform.localPosition;
+        var totalDistance = 0f;
+
+        while (true)
+        {
+            var scaledVelocity = velocity * speedScale;
+            var currVelocity = scaledVelocity * Time.deltaTime;
+
+            totalDistance += currVelocity.magnitude;
+            if (totalDistance > distance)
+            {
+                if (handler != null)
+                {
+                    handler();
+                }
+                yield break;
+            }
+            gameObject.transform.localPosition += currVelocity;
+
+            yield return null;
+        }
+    }
+
+    private IEnumerator MoveByCoroutine(Vector3 velocity)
+    {
+        var startingPosition = gameObject.transform.localPosition;
+        var totalDistance = 0f;
+
+        while (true)
+        {
+            var scaledVelocity = velocity * speedScale;
+            var currVelocity = scaledVelocity * Time.deltaTime;
+
+            totalDistance += currVelocity.magnitude;
+            gameObject.transform.localPosition += currVelocity;
+
+            yield return null;
+        }
+    }
+}
---
+++ Spawner.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+    public SpriteRenderer[] objectsToSpawn;
+    public Vector3 SpawnAreaSize;
+    public float frequencyMin;
+    public float frequencyMax;
+    public int quantityMin;
+    public int quantityMax;
+    public float scaleMin;
+    public float scaleMax;
+    public int depthMin;
+    public int depthMax;
+    public float depthScale;
+    public bool alphaScaleEnabled;
+    
+    public Vector3 moveDirection;
+    public float moveSpeedMin;
+    public float moveSpeedMax;
+    public float stoppingDistance;
+    public OnStop onStop = (x) => Destroy(x);
+
+    public delegate void OnStop(GameObject spawnee);
+
+    private bool isSpawning;
+
+    // Use this for initialization
+    void Start () {
+        StartSpawning();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartSpawning() {
+        isSpawning = true;
+        StartCoroutine(Spawn());
+    }
+
+    public void StopSpawning(bool destroyCurrent) {
+        isSpawning = false;
+        StopCoroutine(Spawn());
+
+        if (destroyCurrent) {
+            foreach (Move move in transform.
+                     GetComponentsInChildren<Move>()) {
+                Destroy(move.gameObject);
+            }
+        }
+    }
+
+    private IEnumerator Spawn() {
+        var halfSize = SpawnAreaSize / 2;
+
+        var normalizedDirection = moveDirection.normalized;
+        var projection = Mathf.Abs(Vector3.Dot(normalizedDirection, halfSize)) * 
+                                normalizedDirection;
+
+        while (isSpawning) {
+            for (int i = 0; i < Random.Range(quantityMin, quantityMax + 1); i++) {
+                var objectToSpawn = objectsToSpawn[Random.Range(0, objectsToSpawn.
+                                                                GetUpperBound(0))];
+                var spawnee = Instantiate(objectToSpawn).gameObject;
+                spawnee.transform.parent = transform;
+
+                var depth = Random.Range(depthMin, depthMax);
+
+                var scale = Random.Range(scaleMin, scaleMax);
+                spawnee.transform.localScale = Vector3.one * scale * Mathf.Pow(depthScale,
+                                                                                 depth);
+
+                var pos = new Vector3(Random.Range(-halfSize.x, halfSize.x),
+                                      Random.Range(-halfSize.y, halfSize.y),
+                                      Random.Range(-halfSize.z, halfSize.z));
+                spawnee.transform.localPosition = pos - projection;
+
+                var spriteRenderer = spawnee.GetComponent<SpriteRenderer>();
+                spriteRenderer.sortingOrder -= depth;
+                var extentProj = 
+                    Mathf.Abs(Vector3.Dot(spriteRenderer.bounds.extents, 
+                                          normalizedDirection)) 
+                         * normalizedDirection;
+                spawnee.transform.localPosition -= extentProj;
+
+                if (alphaScaleEnabled)
+                {
+                    var color = spriteRenderer.color;
+                    color.a *= Mathf.Pow(depthScale, depth);
+                    spriteRenderer.color = color;
+                }
+
+                var speed = Random.Range(moveSpeedMin, moveSpeedMax);
+
+                var maxDistance = stoppingDistance + (extentProj.magnitude * 2)
+                                    + projection.magnitude;
+
+                var move = spawnee.AddComponent<Move>();
+                move.speedScale = Mathf.Pow(depthScale, depth);
+                move.MoveBy(normalizedDirection * speed, maxDistance, () => onStop(spawnee));
+            }
+
+            yield return new WaitForSeconds(Random.Range(frequencyMin, frequencyMax));
+        }
+    }
+
+}
